f0b4f3008ae4fdb37f1d5d674833c7e8a17566f5
hary rabarijoely <rabonia@hotmail.com>
2022-04-02 13:23:17 +0200

    first config and adding pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import React from "react"
+import { Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from "styled-components"
+import { LightTheme } from "./components/Themes"
+import GlobalStyle from "./globalStyles"
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Blog from "./pages/Blog";
+import Work from "./pages/Work";
+import Skill from "./pages/Skill";
+
+
 function App() {
-  return <div>
-    App File
-    </div>
+  return <>
+
+    <GlobalStyle />
+
+      <ThemeProvider theme={LightTheme}>
+        
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/about" element={<About />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/work" element={<Work />} />
+            <Route path="/skill" element={<Skill />} />
+          </Routes>
+        
+      </ThemeProvider>
+    
+    </>
     
 }
 
