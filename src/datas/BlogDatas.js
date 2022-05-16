14248b472d50bd3e2c7b823e0932d1fb4de93210
hary rabarijoely <rabonia@hotmail.com>
2022-05-17 00:13:57 +0200

    adding blog content

---
+++ src/datas/BlogDatas.js
@@ -0,0 +1,69 @@
+export const Blogs = [
+    {
+        id:1,
+        name:"Build Website with ReactJS, Styled-components and GSAP for Scrolling Animations",
+        tags:["react","gsap","styled-components"],
+        date:"16 May, 2022",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
+        link:" "
+    },
+    {
+        id:2,
+        name:"How to create React Hamburger Menu using Styled-Components",
+        tags:["react","styled-components"],
+        date:"16 May, 2022",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
+        link:" "
+    },
+    {
+        id:3,
+        name:"React Loading Screen: Try these 3 cool loading screens for your app",
+        tags:["react","react-lottie","styled-components"],
+        date:"16 May, 2022",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
+        link:" "
+    },
+    {
+        id:4,
+        name:"How to build a Fabulous Todo App using React, Redux and Framer-Motion",
+        tags:["react","redux","framer-motion"],
+        date:"16 May, 2022",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
+        link:" "
+    },
+    {
+        id:5,
+        name:"What is Higher Order Component (HOC) in React JS?",
+        tags:["react","reactJS","web-development"],
+        date:"16 May, 2022",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
+        link:" "
+    },
+    {
+        id:6,
+        name:"How to implement Pagination Component in React from scratch",
+        tags:["react","css"],
+        date:"16 May, 2022",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
+        link:" "
+
+    },
+   
+    {
+        id:7,
+        name:"What is Redux ? Simply Explained!",
+        tags:["react","redux","javascript"],
+        date:"16 May, 2022",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
+        link:" "
+    },
+    {
+        id:8,
+        name:"How to create Sidebar navigation menu in ReactJS with react router and framer-motion",
+        tags:["react","framer-motion","styled-components"],
+        date:"16 May, 2022",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
+        link:" "
+    },
+  
+] 
